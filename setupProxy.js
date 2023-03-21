000f11a1039114fd04add69d35334e5080f335eb
SimratV0 <simratvirk6@hotmail.com>
2023-03-21 11:36:40 -0600

    Proxy works as intended

--- setupProxy.js
+++ setupProxy.js
@@ -1,7 +1,7 @@
-const net = require('net');
+var http = require('http');
+var httpProxy = require('http-proxy');
 
-const server = net.createServer();
-
+var proxy = httpProxy.createProxyServer();
 const servers = {
   srv1: 8080,
   srv2: 5000
@@ -13,76 +13,18 @@ function changeServer(proxyToServerSocket) {
 
 }
 
-server.on('connection', (clientToProxySocket) => {
-  console.log('A Client Connected To Proxy');
-  clientToProxySocket.once('data', (data) => {
 
-    let tls = data.toString().indexOf('CONNECT') !== -1;
 
-    let serverPort = 80; //default
-    let serverAddress;
-    if (tls) {
-      serverPort = data.toString()
-                          .split('CONNECT ')[1].split(' ')[0].split(':')[1];;
-      serverAddress = data.toString()
-                          .split('CONNECT ')[1].split(' ')[0].split(':')[0];
-    } else {
-      serverAddress = data.toString().split('Host: ')[1].split('\r\n')[0];
-    }
-
-    console.log(serverAddress);
+http.createServer(function(req, res){
+		proxy.web(req, res, {target:'http://localhost:8080'});	
+	
+}).listen(9000, () => console.log(`Listening on port 9000`));
 
 
 
-    let proxyToServerSocket = net.createConnection({
-      host: "localhost",
-      port: 8080
-    }, () => {
-      console.log('PROXY TO SERVER SET UP');
-      if (tls) {
-        clientToProxySocket.write('HTTP/1.1 200 OK\r\n\n');
-      } else {
-        proxyToServerSocket.write(data);
-      }
-
-      clientToProxySocket.pipe(proxyToServerSocket);
-      proxyToServerSocket.pipe(clientToProxySocket);
-
-      
-
-      proxyToServerSocket.on('error', (err) => {
-        console.log('PROXY TO SERVER ERROR');
-        console.log(err);
-      });
-
-      
-      
-    });
-
-    // --------- For Fault Tolerance ------------- //
+// --------- For Fault Tolerance ------------- //
     // proxyToServerSocket.setTimeout(3000);
     // proxyToServerSocket.on('timeout', () => {
     //   console.log("timeout error from: Server " + master_server.toString());
     //   changeServer(proxyToServerSocket);
     // });
-
-    clientToProxySocket.on('error', err => {
-      console.log('CLIENT TO PROXY ERROR');
-      console.log(err);
-    });
-  });
-});
-
-server.on('error', (err) => {
-  console.log('SERVER ERROR');
-  console.log(err);
-  throw err;
-});
-
-server.on('close', () => {
-  console.log('Client Disconnected');
-});
-
-server.listen(9000, () => {
-  console.log('Server running at http://localhost:' + 9000);
-});
